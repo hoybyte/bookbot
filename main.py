df2573f7cb47673df7be2bf187881d726f8557fd
hoybyte <hoybyte@gmail.com>
2024-03-07 07:33:48 -0500

    Added letter count function

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    letter_frequencies = get_letter_count(text)
+    print(letter_frequencies)
+    print(f" --- Begin report for {book_path} ---")
     print(f"{num_words} words found in book")
-
 
 def get_book_text(path):
     with open(path) as f:
@@ -14,4 +16,18 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
+def get_letter_count(text):
+    letter_count = {}
+    book_text = text.lower()
+
+    for letter in book_text:
+        if letter.isalpha():
+            letter_count[letter] = letter_count.get(letter, 0) + 1
+    
+    letter_count = dict(sorted(letter_count.items(), key=lambda item: item[1], reverse=True))
+    return letter_count
+
+def generate_report(text):
+    return
+    
 main()
