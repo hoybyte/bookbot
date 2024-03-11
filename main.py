22941b7e13eca2f68e028de8152b262a399d8058
hoybyte <hoybyte@gmail.com>
2024-03-10 20:08:34 -0400

    Added Generate Report Function to main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     letter_frequencies = get_letter_count(text)
-    print(letter_frequencies)
-    print(f" --- Begin report for {book_path} ---")
-    print(f"{num_words} words found in book")
+    generate_report(book_path, num_words, letter_frequencies)
+    
 
 def get_book_text(path):
     with open(path) as f:
@@ -27,7 +26,13 @@ def get_letter_count(text):
     letter_count = dict(sorted(letter_count.items(), key=lambda item: item[1], reverse=True))
     return letter_count
 
-def generate_report(text):
-    return
+def generate_report(book_path, num_words, letter_frequencies):
+    print(f" --- Begin report for {book_path} ---")
+    print(f"{num_words} words found in book")
+
+    for letter in letter_frequencies:
+        print(f"The {letter} character was found {letter_frequencies[letter]} times")
+
+    print(f"--- End Report ---")
     
 main()
